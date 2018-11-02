9b5fc124106b23be72c54170365a4a23d55f334c
tijs <tijs.fertinel.u8681@student.hogent.be>
2018-11-02 13:20:39 +0100

    initial commit

---
+++ Attendance/Models/Group.swift
@@ -0,0 +1,19 @@
+//
+//  Group.swift
+//  Attendance
+//
+//  Created by Tijs Fertinel on 31/10/18.
+//  Copyright © 2018 Tijs Fertinel. All rights reserved.
+//
+
+import Foundation
+
+struct Group {
+    
+    var groupName: String
+    
+    init(groupName: String) {
+        self.groupName = groupName
+    }
+    
+}
