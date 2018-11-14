5496146a0a971a3f00748cb6ee8d91885a639b15
tijs <tijs.fertinel.u8681@student.hogent.be>
2018-11-14 15:42:37 +0100

    added the functionality to remove groups from events

---
+++ frontend/Attendance/Views/SingleGroupCell.swift
@@ -0,0 +1,24 @@
+//
+//  SingleGroupCell.swift
+//  Attendance
+//
+//  Created by Tijs Fertinel on 14/11/2018.
+//  Copyright © 2018 Tijs Fertinel. All rights reserved.
+//
+
+import UIKit
+
+class SingleGroupCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
